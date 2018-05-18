a33d20b9a3b1d63c4667c2166d429a61458e3c8c
kukhariev <q_h@inbox.ru>
2018-05-18 13:12:58 +0300

    test: work now

--- test/diskinfo.spec.ts
+++ test/diskinfo.spec.ts
@@ -18,13 +18,14 @@ describe(`${diskinfo.name} test:\n`, () => {
     expect(dfout).to.include.keys('fstype', 'size', 'used', 'avail', 'pcent', 'target');
   });
   it('should fails if file path is wrong', async () => {
+    let error;
     try {
       const dfout = await diskinfo('I`mNotExist');
-      expect(dfout).to.be.undefined;
-    } catch (error) {
-      console.log(error.message);
-      expect(error).to.be.an.instanceof(Error);
-      expect(error.message).to.have.lengthOf.at.least(3);
+    } catch (err) {
+      error = err;
     }
+    expect(error).to.be.an.instanceof(Error);
+    expect(error.message).to.have.lengthOf.at.least(3);
+    console.log(error.message);
   });
 });
