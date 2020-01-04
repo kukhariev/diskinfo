031999785304457e98fc7d14859aada0396ce526
kukhariev <q_h@inbox.ru>
2020-01-05 00:06:00 +0300

    chore: update deps
    BREAKING CHANGE: drop nodejs v6.x support

--- test/diskinfo.spec.ts
+++ test/diskinfo.spec.ts
@@ -6,14 +6,17 @@ describe(`diskinfo`, () => {
     const dfout = await diskinfo();
     expect(dfout[0]).to.include.keys('fstype', 'size', 'used', 'avail', 'pcent', 'target');
   });
+
   it('should get current disk info (folder)', async () => {
     const dfout = await diskinfo('./');
     expect(dfout).to.include.keys('fstype', 'size', 'used', 'avail', 'pcent', 'target');
   });
+
   it('should get current disk info (file)', async () => {
     const dfout = await diskinfo('package.json');
     expect(dfout).to.include.keys('fstype', 'size', 'used', 'avail', 'pcent', 'target');
   });
+
   it('should fails if file path is wrong', async () => {
     let error: Error;
     try {
